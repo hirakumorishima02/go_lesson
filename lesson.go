3588f56a156d7e310bd26d5eef7a65270bf974c7
hirakumorishima02 <business.ujinchu@gmail.com>
2019-11-16 18:54:23 +0900

    Lesson14 配列

--- lesson.go
+++ lesson.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
-	var s string = "14"
-	// Stringからintに直す際にはAtoi関数を使用
-	// Atoiは返り値に n と nil(エラー値)を返す
-	// 第２引数には _ を入れる。errorなどを入れてしまうと「errorが使用されていない」というエラーが出るため
-	i, _ := strconv.Atoi(s)
-	fmt.Printf("%T %v", i, i)
+	var s [2]int
+	s[0] = 100
+	s[1] = 200
+	fmt.Println(s)
+
+	var n []int = []int{100, 200}
+	n = append(n, 300)
+	fmt.Println(n)
 }
