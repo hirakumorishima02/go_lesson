89dcaeff21041db69dae1791db94db241e57fdca
hirakumorishima02 <business.ujinchu@gmail.com>
2019-11-16 19:19:57 +0900

    Lesson 15 スライス

--- lesson.go
+++ lesson.go
@@ -5,12 +5,14 @@ import (
 )
 
 func main() {
-	var s [2]int
-	s[0] = 100
-	s[1] = 200
-	fmt.Println(s)
-
-	var n []int = []int{100, 200}
-	n = append(n, 300)
-	fmt.Println(n)
+	// 配列を入れ子にするときには[][]と2つ書く
+	// 代入された配列([]int{0,1,2}...など）は1つ目の[]に入る
+	var board = [][]int{
+		[]int{0, 1, 2},
+		[]int{3, 4, 5},
+		[]int{6, 7, 8},
+	}
+	fmt.Println(board)
+	board = append(board, []int{9, 10, 11}, []int{12, 13, 14})
+	fmt.Println(board)
 }
