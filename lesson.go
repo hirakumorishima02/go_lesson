dc703c656de650172ed8b16dcfd3148f771de7f4
hirakumorishima02 <business.ujinchu@gmail.com>
2019-11-16 19:42:51 +0900

    Lesson16 スライスのmakeとcap

--- lesson.go
+++ lesson.go
@@ -5,14 +5,17 @@ import (
 )
 
 func main() {
-	// 配列を入れ子にするときには[][]と2つ書く
-	// 代入された配列([]int{0,1,2}...など）は1つ目の[]に入る
-	var board = [][]int{
-		[]int{0, 1, 2},
-		[]int{3, 4, 5},
-		[]int{6, 7, 8},
+	c := make([]int, 0, 5)
+	for i := 0; i < 5; i++ {
+		c = append(c, i)
+		fmt.Println(c)
 	}
-	fmt.Println(board)
-	board = append(board, []int{9, 10, 11}, []int{12, 13, 14})
-	fmt.Println(board)
+	fmt.Println(c)
+
+	d := make([]int, 5)
+	for i := 0; i < 5; i++ {
+		d = append(d, i)
+		fmt.Println(d)
+	}
+	fmt.Println(d)
 }
