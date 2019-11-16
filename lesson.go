2daaafed8bf666709817177dc52085e83405b6a6
hirakumorishima02 <business.ujinchu@gmail.com>
2019-11-16 18:47:39 +0900

    Lesson13 型変換

--- lesson.go
+++ lesson.go
@@ -2,16 +2,14 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"strconv"
 )
 
 func main() {
-	var s string = "Hello World"
-	fmt.Println(strings.Replace(s, "o", "X", 2))
-
-	fmt.Println(`Test
-	test
-						test`)
-	fmt.Println("\"")
-	fmt.Println(`"`)
+	var s string = "14"
+	// Stringからintに直す際にはAtoi関数を使用
+	// Atoiは返り値に n と nil(エラー値)を返す
+	// 第２引数には _ を入れる。errorなどを入れてしまうと「errorが使用されていない」というエラーが出るため
+	i, _ := strconv.Atoi(s)
+	fmt.Printf("%T %v", i, i)
 }
